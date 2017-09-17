a2a1ab0687b38e92f88e0774bfd6af4733353983
Milan Kopsa <wikan@wikan.com>
2017-09-17 18:02:48 +0200

    Moved Solution class to Solution.cs

---
+++ Core/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Astroants.Core
+{
+    public class Solution
+    {
+        public int Duration { get; set; }
+        public List<Direction> Path { get; set; }
+    }
+}
--- Core/Solver.cs
+++ Core/Solver.cs
@@ -71,10 +71,4 @@ namespace Astroants.Core
             };
         }
     }
-
-    public class Solution
-    {
-        public int Duration { get; set; }
-        public List<Direction> Path { get; set; }
-    }
 }
